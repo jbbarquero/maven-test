e4fb30128cc7aa3de8ce72e3dd79b6a007d809d4
Javier Beneito Barquero <jbbarquero@jbbarquero.gmail.com>
2014-01-29 17:50:44 +0100

    First commit: project created

---
+++ src/main/java/com/malsolo/maven/test/App.java
@@ -0,0 +1,13 @@
+package com.malsolo.maven.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
